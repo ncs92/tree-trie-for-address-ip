0eb69d3be140bb3abb7eb2603bfe1fce4a405554
ncs92 <e.nani92@gmail.com>
2015-12-13 15:35:20 -0200

    ...

--- Trie/src/classes/Arvore.java
+++ Trie/src/classes/Arvore.java
@@ -13,25 +13,47 @@ public class Arvore {
 
     private static final Node raiz = new Node();
 
-    private static Node buscaNoEsquerda(Node raiz) {
-         if(raiz.getEsquerda() == null){
-             return raiz;
-         }else{
-             buscaNoEsquerda(raiz.getEsquerda());
-         }
-        
-        return raiz ;
-    }
-    private static Node buscaNoDireita(Node raiz) {
-         if(raiz.getDireita() == null){
-             return raiz;
-         }else{
-             buscaNoDireita(raiz.getDireita());
-         }
-        
-        return raiz;
+    private static Node buscaNoEsquerda(Node raiz, String b) {
+        Node no = new Node();
+
+        if (raiz.getEsquerda() == null) {
+            no = raiz;
+        } else {
+
+            buscaNoEsquerda(raiz.getEsquerda(), b);
+        }
+        no.setValor(String.valueOf(somaElementosE(raiz))+b);
+        return no;
     }
 
+    private static Node buscaNoDireita(Node raiz, String b) {
+        Node no = new Node();
+
+        if (raiz.getDireita() == null) {
+            no = raiz;
+        } else {
+
+            buscaNoDireita(raiz.getDireita(), b);
+        }
+        no.setValor(String.valueOf(somaElementosD(raiz))+b);
+        return no;
+    }
+
+    private static String somaElementosD(Node raiz) {
+        if (raiz != null) {
+            String soma = Arvore.somaElementosD(raiz.getDireita()) + raiz.getValor();
+            return soma;
+        }
+        return null;
+    }
+
+    private static String somaElementosE(Node raiz) {
+        if (raiz != null) {
+            String soma = Arvore.somaElementosE(raiz.getEsquerda()) + raiz.getValor();
+            return soma;
+        }
+        return null;
+    }
 
     public Arvore() {
         if (raiz == null) {
@@ -44,39 +66,32 @@ public class Arvore {
      *
      * @param binario
      */
-    public static void insereNo(String binario) {        
+    public static void insereNo(String binario) {
         for (int i = 0; i < binario.length(); i++) {
             int b = Integer.parseInt(String.valueOf(binario.charAt(i)));
-            
-          
+
             if (b == 0) {
-                if(raiz.getEsquerda() == null){
-                     Node no = new Node();
-                    no.setValor(String.valueOf(b));
-                    raiz.setEsquerda(no);                   
-                }else{
-                     Node no = new Node();
-                    no = buscaNoEsquerda(raiz);
-                    String valor = no.getEsquerda().getValor();
-                    valor += b;
-                    buscaNoEsquerda(raiz).getEsquerda().setValor(valor);
-                }
-            } else {
-                if(raiz.getDireita() == null){
+                if (raiz.getEsquerda() == null) {
                     Node no = new Node();
                     no.setValor(String.valueOf(b));
-                    raiz.setDireita(no);                       
-                }else{
-                   Node no = new Node();
-                    no = buscaNoDireita(raiz);
-                    String valor = no.getDireita().getValor();
-                    valor += b;
-                    buscaNoDireita(raiz).getDireita().setValor(valor);
+                    raiz.setEsquerda(no);
+                } else {
+                    Node no = new Node();
+                    no = buscaNoEsquerda(raiz, String.valueOf(b));
+                }
+            } else {
+                if (raiz.getDireita() == null) {
+                    Node no = new Node();
+                    no.setValor(String.valueOf(b));
+                    raiz.setDireita(no);
+                } else {
+                    Node no = new Node();
+                    no = buscaNoDireita(raiz,String.valueOf(b));  
                 }
             }
 
         }
-
+        imprime(raiz);
     }
 
     public void removeNo() {
@@ -84,6 +99,17 @@ public class Arvore {
     }
 
     public void buscaNo() {
+
+    }
+
+    public static void imprime(Node raiz) {
+        if (raiz == null) {
+            return;
+        } else {
+            System.out.println("\n " + raiz.getValor());
+            imprime(raiz.getEsquerda());
+            imprime(raiz.getDireita());
+        }
 
     }
 }
